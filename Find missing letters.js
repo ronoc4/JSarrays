51cb28d549dcda3046651471bc14ce42ab15219a
conor <conorbudge555@gmail.com>
2016-08-17 16:02:58 -0500

    Find missing letter in alphabet

---
+++ Find missing letters.js
@@ -0,0 +1,28 @@
+/**
+ * Created by conor on 8/17/16.
+ */
+
+function fearNotLetter(str) {
+    //Create alphabet string
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    str = str.toLowerCase();
+    alphabet = alphabet.toLowerCase();
+    //Split into array
+    alphabet = alphabet.split("");
+    str = str.split("");
+
+    if(str[0] != "a") {
+        return undefined;
+    }
+
+    for(var i = 0; i < str.length; i++) {
+        if(str[i] != alphabet[i]) {
+            return alphabet[i];
+        }
+    }
+
+    return str;
+}
+
+console.log(fearNotLetter("Abce"));
