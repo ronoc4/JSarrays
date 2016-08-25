8334eb7585b8dc43c23c48a3f8ce52ef8d41bd07
conor <conorbudge555@gmail.com>
2016-08-25 14:51:08 -0500

    Working with Regrex

---
+++ Regrex.js
@@ -0,0 +1,20 @@
+/**
+ * Created by conor on 8/25/16.
+ */
+
+
+function spinalCase(str) {
+    // "It's such a fine line between stupid, and clever."
+    // --David St. Hubbins
+
+    //Remove whitespace and underscores from string
+    var newStr = (/\s+|_/g);
+
+    //Replace lower case letters at pos 1, 2 with uppercase
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+    //Split white space with dash
+    return final = str.replace(newStr, '-').toLowerCase();
+}
+
+console.log(spinalCase('AllThe-small Things' + ' This_isAn other Example'));
