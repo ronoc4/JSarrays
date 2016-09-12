b4ff6069165240e93484e5e133ea34d0287e7184
conor <conorbudge555@gmail.com>
2016-09-12 16:00:57 -0500

    added some comments

--- common multiples.js
+++ common multiples.js
@@ -5,26 +5,31 @@
 
 function range(rangeArray) {
     var array = [];
-
+    //Finds highest and lowest values in array
     var high = rangeArray.reduce(function(a,b){
         return Math.max(a,b);
     });
     var low = rangeArray.reduce(function(a,b){
         return Math.min(a,b);
     });
-
+    //Loops through lowest and highest values to create array of range of values
     for(var i = low; i <= high; i++){
         array.push(i);
     }
     var x = true;
     var lcm = 0;
-    //noprotect
+
+
+    //While loop to increment each iteration of lcm(lowest common multiple)
     while(x) {
         lcm++;
         for(var j = array[0]; j <= array[array.length -1]; j++){
+            //Ends for loop if lcm is not 0
             if(lcm % j !== 0) {
                 break;
+                //when you reach end of array it has passed the lcm
             } else if (j == array[array.length - 1]){
+                //break the while loop
                 x = false;
             }
         }
